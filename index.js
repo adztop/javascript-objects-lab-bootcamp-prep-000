af7b5bd0c45735be879a9ccd982bc34cbe12ac6f
Rinoa Heartilly <kissy0227@yahoo.com.au>
2018-08-09 19:26:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function deleteFromObjectByKey(object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  var newObject = delete object.key
+  var newObject = Object.assign({}, object, { [key]: value })
+  newObject = delete object.key
   return newObject
 }
